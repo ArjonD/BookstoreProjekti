c5718e208d2bf083cec152fe0b90820af0109c01
ArjonD <bhm527@myy.haaga-helia.fi>
2025-02-09 22:04:12 +0200

    Delete ja add lisätty

--- src/main/java/hh/backend/bookstore/web/BookController.java
+++ src/main/java/hh/backend/bookstore/web/BookController.java
@@ -4,8 +4,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import hh.backend.bookstore.domain.Book;
 import hh.backend.bookstore.domain.BookRepository;
 
 
@@ -20,6 +25,22 @@ public class BookController {
         model.addAttribute("books", bookRepository.findAll());
         return "booklist";
     }
+
+    @GetMapping("/addbook")
+    public String showAddBookForm(Model model) {
+        model.addAttribute("book", new Book());
+        return "addbook";
+    }
     
-    
+    @PostMapping("/books/delete/{id}")
+    public String deleteBook(@PathVariable("id") Long bookId) {
+        bookRepository.deleteById(bookId);
+        return "redirect:/index";
+    }
+
+    @PostMapping("/addbook")
+    public String addBook(@ModelAttribute Book book) {
+        bookRepository.save(book);
+        return "redirect:/index";
+    }
 }
