88f535e3c7b2cfecaf73098e7a2144b77d89610f
ArjonD <bhm527@myy.haaga-helia.fi>
2025-02-09 17:12:19 +0200

    booklist tehty

--- src/main/java/hh/backend/bookstore/web/BookController.java
+++ src/main/java/hh/backend/bookstore/web/BookController.java
@@ -1,18 +1,23 @@
 package hh.backend.bookstore.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import hh.backend.bookstore.domain.BookRepository;
+
 
 @Controller
 public class BookController {
 
+    @Autowired
+    private BookRepository bookRepository;
 
     @GetMapping("/index")
     public String GetBooks(Model model) {
-        
+        model.addAttribute("books", bookRepository.findAll());
         return "booklist";
     }
     
